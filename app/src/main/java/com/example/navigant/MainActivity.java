9bad2d435d3c3cc1f679634b8902672a1365b3fa
namhkoh <43123189+namhkoh@users.noreply.github.com>
2020-10-08 20:19:12 +0100

    Configs.

--- app/src/main/java/com/example/navigant/MainActivity.java
+++ app/src/main/java/com/example/navigant/MainActivity.java
@@ -2,8 +2,22 @@ package com.example.navigant;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "MainActivity";
+
+    static
+    {
+        if(OpenCVLoader.initDebug()){
+            Log.d(TAG,"OpenCV is Configured or Connected Successfully.");
+        } else {
+            Log.d(TAG,"OpenCV not working or loaded properly");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

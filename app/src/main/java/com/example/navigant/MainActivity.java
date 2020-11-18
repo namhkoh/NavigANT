37b1eae711225895fb21a2729992a4c09e11b361
namhkoh <43123189+namhkoh@users.noreply.github.com>
2020-11-18 16:21:00 +0000

    Added test method.

--- app/src/main/java/com/example/navigant/MainActivity.java
+++ app/src/main/java/com/example/navigant/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.util.Log;
 
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,5 +26,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tester();
+    }
+
+    public void tester(){
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
+        System.out.println("mat = " + mat.dump());
     }
 }
